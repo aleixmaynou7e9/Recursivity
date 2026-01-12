2ca9161a68921841935ac7721095fb4bd9690f7e
Aleix <aleix.maynou.7e9@itb.cat>
2026-01-12 12:18:08 +0100

    maiami me lo confirmo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Countdown(7);
+    }
+    public static void Countdown(int n)
+    {
+        if (n < 0) return;
+
+        Console.WriteLine(n);
+        Countdown(n - 1);
+    }
+}
